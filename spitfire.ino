23506ea4f687f65931fd65794e0134b8c9ad2c38
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-07-14 01:11:02 +0200

    Initial commit
    with the arduino file

---
+++ spitfire.ino
@@ -0,0 +1,42 @@
+#define GEAR 2
+
+#define INPUT_COUNT 1
+#define KEYMAP "g"
+
+int inputs[INPUT_COUNT] = {
+  2
+};
+
+int states[INPUT_COUNT];
+
+char keymap[INPUT_COUNT * 2] = {
+  'd', 'g'
+};
+
+void setup() {
+  Serial.begin(9600);
+  
+  for (int i = 0; i < INPUT_COUNT; ++i) {
+    pinMode(inputs[i], INPUT);
+    states[i] = readState(inputs[i]);
+  }
+  
+  // Wait for serial connection to be established
+  while (!Serial);
+}
+
+int readState(int pin) {
+  return digitalRead(pin);
+}
+
+void loop() {
+  for (int i = 0; i < INPUT_COUNT; ++i) {
+    int pin = inputs[i];
+    int val = readState(pin);
+    
+    if (val != states[i]) {
+      states[i] = val;
+      Serial.print(keymap[i * 2 + val == HIGH]);
+    }
+  }
+}
