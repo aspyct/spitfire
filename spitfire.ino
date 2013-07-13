44a116fee3558a41db9fc0b87ccf6af31f71cb8f
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-07-14 01:20:07 +0200

    Added a timeguard to prevent intensive triggering

--- spitfire.ino
+++ spitfire.ino
@@ -1,5 +1,5 @@
-
 #define INPUT_COUNT 1
+#define TIMEGUARD 100 // numper of milliseconds before a new input is taken into account
 
 int inputs[INPUT_COUNT] = {
   2
@@ -11,12 +11,15 @@ char keymap[INPUT_COUNT * 2] = {
   'd', 'g'
 };
 
+unsigned long fired[INPUT_COUNT];
+
 void setup() {
   Serial.begin(9600);
   
   for (int i = 0; i < INPUT_COUNT; ++i) {
     pinMode(inputs[i], INPUT);
     states[i] = readState(inputs[i]);
+    fired[i] = 0;
   }
   
   // Wait for serial connection to be established
@@ -24,16 +27,19 @@ void setup() {
 }
 
 int readState(int pin) {
-  return digitalRead(pin);
+  return digitalRead(pin)ßß;
 }
 
 void loop() {
   for (int i = 0; i < INPUT_COUNT; ++i) {
+    unsigned long time = millis();
+    
     int pin = inputs[i];
     int val = readState(pin);
     
-    if (val != states[i]) {
+    if (val != states[i] && time - fired[i] > TIMEGUARD) {
       states[i] = val;
+      fired[i] = time;
       Serial.print(keymap[i * 2 + val == HIGH]);
     }
   }
