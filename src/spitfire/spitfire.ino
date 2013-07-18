9d1d1e3a1ba0a56665c187ade1eb55e6121c7e81
Antoine d Otreppe <aob@emakina.com>
2013-07-18 10:25:42 +0200

    Added the license notice in the source and moved the source to the correct directory

--- src/spitfire/spitfire.ino
+++ src/spitfire/spitfire.ino
@@ -1,3 +1,21 @@
+/*
+ * Spitfire board is a plane simulation control board
+ * Copyright (C) 2013  Antoine d'Otreppe de Bouvette
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
 #define INPUT_COUNT 2
 #define TIMEGUARD 100 // numper of milliseconds before a new input is taken into account
 
